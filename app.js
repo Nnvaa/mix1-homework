0bbf2727a3ee7168802760a612b1796ccdc04379
Nnvaa <nigarnva2005@gmail.com>
2025-04-04 20:48:05 +0400

    nnn

---
+++ app.js
@@ -0,0 +1,23 @@
+
+
+let table = document.querySelector("table");
+let tr = document.createElement("tr");
+tr.innerHTML = "<td>Amin Nifteliyev</td>";
+table.append(tr);
+tr.style.backgroundColor = "white"
+
+
+let nameee = document.createElement("tr");
+table.append(nameee);
+nameee.innerHTML = "<td>Medine Cabbarli</td>";
+nameee.style.backgroundColor = "rgb(172, 172, 252)"
+
+
+
+let namee = document.createElement("tr");
+namee.innerHTML = "<td>Daniz Qudretli</td>";
+table.append(namee);
+namee.style.backgroundColor = "white"
+
+
+
